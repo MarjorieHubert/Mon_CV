ddcc9cb271bccef05e272a5634c04b1c0dfdba6a
Marjorie Hubert <92445093+MarjorieHubert@users.noreply.github.com>
2022-11-16 15:49:37 +0100

    Media queries

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 AOS.init({
     duration: 1000
    });
+
+
+   $( document ).ready(function() {
+    $(".nav-link").each(function() {
+        
+        $(this).click(function() {
+            
+            $('#navbarNavAltMarkup').collapse("hide");
+        });
+
+        
+    });
+  });
